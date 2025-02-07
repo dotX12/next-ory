3a4bff8193a2ae54a1975854f1145a1ecad67306
dotX12 <dev@shitposting.team>
2025-02-07 23:18:04 +0300

    ✨ feat(`authentication/src/app/flow/consent/page.tsx`): Add new consent page with React and ORY SDK integration.

--- authentication/src/app/flow/consent/page.tsx
+++ authentication/src/app/flow/consent/page.tsx
@@ -1,90 +1,100 @@
-'use server';
+import React from 'react'
+import {Card} from '@/components/ui/card'
+import {OAuth2ConsentRequest} from '@ory/client'
+import ConsentForm from '@/components/consentForm'
+import {getFrontendApi, getOAuth2Api} from '@/ory/sdk/server'
+import {redirect} from 'next/navigation'
+import {extractSession, shouldSkipConsent} from "@/ory";
+import {cookies} from "next/headers";
 
-import React from 'react';
-import { Card } from '@/components/ui/card';
-import { OAuth2ConsentRequest, OAuth2RedirectTo } from '@ory/client';
-import ConsentForm from '@/components/consentForm';
-import { redirect } from 'next/navigation';
-import { toast } from 'sonner';
-import { getOAuth2Api } from '@/ory/sdk/server';
+export const dynamic = 'force-dynamic';
 
-export default async function Consent(props: { searchParams: Promise<{ consent_challenge: string }> }) {
+interface ConsentPageProps {
+    consent_challenge: string
+}
 
-    const searchParams = await props.searchParams;
+export default async function ConsentPage(props: {
+    searchParams: Promise<ConsentPageProps>
+}) {
+    const serviceSearchParams = await props.searchParams;
+    console.debug("serviceSearchParams: ", serviceSearchParams);
 
-    const consentChallenge = searchParams.consent_challenge ?? undefined;
-    let consentRequest: OAuth2ConsentRequest | undefined = undefined;
+    const challenge = String(serviceSearchParams.consent_challenge);
+    console.debug("consentChallenge: ", challenge);
 
-    const onAccept = async (challenge: string, scopes: string[], remember: boolean) => {
-        'use server';
+    if (!challenge) {
+        return <div>Invalid consent challenge</div>
+    }
 
-        const hydra = await getOAuth2Api();
-        const response = await hydra
-            .acceptOAuth2ConsentRequest({
+    let hydra
+    let frontend
+
+    try {
+        hydra = await getOAuth2Api()
+    } catch (error) {
+        console.error('Could not init Hydra', error)
+        return <div>Could not init Hydra</div>
+    }
+    try {
+        frontend = await getFrontendApi()
+    } catch (error) {
+        console.error('Could not init Frontend', error)
+        return <div>Could not init Frontend</div>
+    }
+
+    let consent: OAuth2ConsentRequest
+    try {
+        const consentResponse = await hydra.getOAuth2ConsentRequest(
+            {consentChallenge: challenge},
+        )
+        consent = consentResponse.data
+    } catch (error) {
+        console.error('Failed to fetch consent request', error)
+        return <div>Error processing consent challenge</div>
+    }
+
+    // If a user has granted this application the requested scope, hydra will tell us to not show the UI.
+    if (shouldSkipConsent(consent)) {
+        console.debug("shouldSkipConsent: ", consent)
+
+        let grantScope = consent.requested_scope || [];
+
+        let h = await cookies()
+        let oryKratosSession = h.get("ory_kratos_session")?.value
+        console.debug("ory_kratos_session: ", oryKratosSession);
+
+        const sessionResponse = (
+            await frontend.toSession({
+                cookie: `ory_kratos_session=${oryKratosSession}`,
+            })
+        ).data;
+
+        const acceptedSession = (
+            await hydra.acceptOAuth2ConsentRequest({
                 consentChallenge: challenge,
                 acceptOAuth2ConsentRequest: {
-                    grant_scope: scopes,
-                    remember: remember,
-                    remember_for: 3600,
+                    // We can grant all scopes that have been requested - hydra already checked for us that no additional scopes
+                    // are requested accidentally.
+                    grant_scope: grantScope,
+
+                    // ORY Hydra checks if requested audiences are allowed by the client, so we can simply echo this.
+                    grant_access_token_audience:
+                    consent.requested_access_token_audience,
+
+                    // The session allows us to set session data for id and access tokens
+                    session: extractSession(sessionResponse.identity, grantScope),
                 },
             })
-            .then(({ data }) => data)
-            .catch((_) => {
-                toast.error('Something unexpected went wrong.');
-            });
-
-        if (!response) {
-            return redirect('/');
-        }
-
-        return redirect(response.redirect_to);
-    };
-
-    const onReject = async (challenge: string) => {
-        'use server';
-
-        const hydra = await getOAuth2Api();
-        const response: OAuth2RedirectTo | void = await hydra
-            .rejectOAuth2ConsentRequest({
-                consentChallenge: challenge,
-            })
-            .then(({ data }) => data)
-            .catch((_) => {
-                toast.error('Something unexpected went wrong.');
-            });
-
-        if (!response) {
-            return redirect('/');
-        }
-
-        return redirect(response.redirect_to);
-    };
-
-    if (!consentChallenge) {
-        return;
+        ).data;
+        redirect(acceptedSession.redirect_to);
     }
-
-    const hydra = await getOAuth2Api();
-    await hydra
-        .getOAuth2ConsentRequest({ consentChallenge })
-        .then(({ data }) => {
-            if (data.skip) {
-                onAccept(consentChallenge, data.requested_scope!, false);
-                return;
-            }
-            consentRequest = data;
-        });
-
-    if (!consentRequest) {
-        return;
-    }
-
     return (
         <Card className="flex flex-col items-center w-full max-w-sm p-4">
             <ConsentForm
-                request={consentRequest}
-                onAcceptAction={onAccept}
-                onRejectAction={onReject}/>
+                request={consent}
+                consentChallenge={challenge}
+            />
         </Card>
-    );
+    )
+
 }
