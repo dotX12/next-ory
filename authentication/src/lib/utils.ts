16964daeaa73306708a8f03cf3aae67691aeec0e
dotX12 <dev@shitposting.team>
2025-02-07 23:20:50 +0300

    ➕ feat(`authentication/src/lib/utils.ts`): Add `generateCsrfToken` function to generate random CSRF tokens.

--- authentication/src/lib/utils.ts
+++ authentication/src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import {type ClassValue, clsx} from 'clsx';
+import {twMerge} from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+export function generateCsrfToken(): string {
+    // Generate a random CSRF token
+    return Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15);
 }
